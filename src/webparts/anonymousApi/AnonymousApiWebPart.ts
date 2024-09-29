e11f35af781185ff5edc0309592c6815293146cb
rezmes <rezmes@gmail.com>
2024-09-29 17:42:06 +0330

    sucsessfully finished

--- src/webparts/anonymousApi/AnonymousApiWebPart.ts
+++ src/webparts/anonymousApi/AnonymousApiWebPart.ts
@@ -11,6 +11,13 @@ import * as strings from 'AnonymousApiWebPartStrings';
 import AnonymousApi from './components/AnonymousApi';
 import { IAnonymousApiProps } from './components/IAnonymousApiProps';
 
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+// import { response } from './components/IAnonymousApiProps';
+import {HttpClient, HttpClientResponse} from '@microsoft/sp-http';
+
 export interface IAnonymousApiWebPartProps {
   description: string;
 }
@@ -18,23 +25,48 @@ export interface IAnonymousApiWebPartProps {
 export default class AnonymousApiWebPart extends BaseClientSideWebPart<IAnonymousApiWebPartProps> {
 
   public render(): void {
+
+
+    this.getUserDetails()
+    .then((response) => {
+
     const element: React.ReactElement<IAnonymousApiProps > = React.createElement(
       AnonymousApi,
       {
         description: this.properties.description,
-        id: Response.id,
-        name:Response.name,
-        username:Response.username,
-        email: Response.email,
+        id: response.id,
+        name:response.name,
+        username:response.username,
+        email: response.email,
         address: 'Street: ' + response.address.street +' Suite: ' + response.address.suite +' City: ' + response.address.city +' Zipcode: ' + response.address.zipcode,
-        phone: Response.phone,
-        website: Response.website,
+        phone: response.phone,
+        website: response.website,
         company: 'Name: ' + response.company.name +' CatchPhrase: ' + response.company.catchPhrase +' BS: ' + response.company.bs
       }
+
     );
 
     ReactDom.render(element, this.domElement);
   }
+);
+}
+
+  protected onInit(): Promise<void> {
+    return super.onInit().then(_ => {
+      sp.setup({
+        spfxContext: this.context
+      });
+    });
+  }
+
+private getUserDetails(): Promise<any> {
+  return this.context.httpClient.get(`https://jsonplaceholder.typicode.com/users/1`, HttpClient.configurations.v1).then((response: HttpClientResponse) => {
+    return response.json();
+  })
+  .then(jsonResponse => {
+    return jsonResponse
+  }) as Promise<any>;
+}
 
   protected onDispose(): void {
     ReactDom.unmountComponentAtNode(this.domElement);
