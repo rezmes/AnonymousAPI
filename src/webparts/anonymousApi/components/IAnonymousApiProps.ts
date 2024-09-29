e4cc8c5ea11f0fa79dbb06b571416f06160c73a7
rezmes <rezmes@gmail.com>
2024-09-29 13:16:02 +0330

    AnonymousApi start

---
+++ src/webparts/anonymousApi/components/IAnonymousApiProps.ts
@@ -0,0 +1,12 @@
+export interface IAnonymousApiProps {
+  description: string;
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  address: string;
+  phone: string;
+  website: string;
+  company: string;
+}
+
